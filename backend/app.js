1d12858c92e94ca7da336584fbf1e89a9c4cc19b
Hari-preetham-B <haripreetham.1111@gmail.com>
2026-07-24 14:57:24 +0530

    feat(auth): implement user registration with password hashing

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const authRoutes = require("./routes/authRoutes");
+const app = express();
+
+/*
+|--------------------------------------------------------------------------
+| Middlewares
+|--------------------------------------------------------------------------
+*/
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use("/api/auth", authRoutes);
+/*
+|--------------------------------------------------------------------------
+| Health Check Route
+|--------------------------------------------------------------------------
+*/
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "TransitOps API is running 🚚",
+  });
+});
+
+module.exports = app;
---
+++ backend/controllers/authController.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Check if all required fields are provided
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all required fields",
+      });
+    }
+
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Create new user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
